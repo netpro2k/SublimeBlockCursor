f598d33e14ef88fb85b464a889dfbd8c885af020
netpro2k <netpro2k@gmail.com>
2011-12-03 17:13:07 -0500

    Fixed to work with multiple cursors

--- SublimeBlockCursor.py
+++ SublimeBlockCursor.py
@@ -7,18 +7,25 @@ class SublimeBlockCursor(sublime_plugin.EventListener):
         settings = view.settings()
         return bool(settings.get('is_widget'))
 
-    def show_block_cursor(self, view, reg):
-        view.add_regions('SublimeBlockCursorListener', [sublime.Region(reg.a, reg.a + 1)], 'block_cursor')
+    def show_block_cursor(self, view):
+        validRegions = []
+        for s in view.sel():
+            if s.a != s.b:
+                continue
+            validRegions.append(sublime.Region(s.a, s.a + 1))
+        if validRegions.__len__:
+            view.add_regions('SublimeBlockCursorListener', validRegions, 'block_cursor')
+        else:
+            view.erase_regions('SublimeBlockCursorListener')
 
     def on_selection_modified(self, view):
-        reg = view.sel()[0]
-        if view.settings().get('is_widget') or not view.settings().get('command_mode') or reg.a != reg.b:
+        if view.settings().get('is_widget') or not view.settings().get('command_mode'):
             view.erase_regions('SublimeBlockCursorListener')
             return
-        self.show_block_cursor(view, reg)
+        self.show_block_cursor(view)
 
     def on_deactivated(self, view):
         view.erase_regions('SublimeBlockCursorListener')
 
     def on_activated(self, view):
-        self.show_block_cursor(view, view.sel()[0])
+        self.show_block_cursor(view)
